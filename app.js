075baedc7fdc30030d2e8e35265ba4c2c953b39b
MatteoMeza <matteomeza@gmail.com>
2021-10-04 15:12:57 -0700

    added validate method to validated required answers

--- app.js
+++ app.js
@@ -7,11 +7,27 @@ const promptUser = () => {
     {
         type: "input",
         name: "name",
-        message: "What is your name?"
+        message: "What is your name? (Required)",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please enter your name!");
+                return false;
+            }
+        }
     },
     {   type: "input",
         name: "github",
-        message: "Enter your GitHub Username"
+        message: "Enter your GitHub Username (Required)",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please enter you GitHub Username!");
+                return false;
+            }
+        }
 
     },
     {
@@ -36,12 +52,30 @@ const promptProject = portfolioData => {
         {
             type: "input",
             name: "name",
-            message: "What is the name of your project?"
+            message: "What is the name of your project? (Required)",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the name of your project!");
+                    return false;
+                }
+            }
+
         },
         {
             type:"input",
             name: "description",
-            message: "Provide a description of the project (Required)"
+            message: "Provide a description of the project (Required)",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a description of your project!");
+                    return false;
+                }
+            }
+
         },
         {
             type: "checkbox",
@@ -52,7 +86,16 @@ const promptProject = portfolioData => {
         {
             type: "input",
             name: "link",
-            message: "Enter your GitHub link to your project. (Required)"
+            message: "Enter your GitHub link to your project. (Required)",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your GitHub link to your project!");
+                    return false;
+                }
+            }
+
         },
         {
             type: "confirm",
